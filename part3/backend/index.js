b679bfb1a8b451cf3da70786c9abf0359571ce95
Kevin La <kevin.la26@hotmail.com>
2026-02-12 14:17:59 +1100

    part3b

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+
+app.use(express.static('dist'))
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -74,7 +78,7 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 var morgan = require('morgan')
 const app = express()
 
-
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
-// app.use(morgan('tiny'))
 app.use(morgan (
   function (tokens, req, res) {
     console.log(req.body)
